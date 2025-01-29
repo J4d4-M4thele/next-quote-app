a87d1ed4925d2cb08e81d730486e6d97ffbba67a
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-01-29 12:02:46 +0200

    update

--- app/components/Quotes.jsx
+++ app/components/Quotes.jsx
@@ -9,36 +9,57 @@ async function getQuotes() {
   return json;
 }
 
-const Quotes = async () => {
+const Quotes = () => {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    getQuotes()
-    .then((quotes) => {
+    getQuotes().then((quotes) => {
       setQuotes(quotes);
       setLoading(false);
     });
   }, []);
 
   if (loading) {
-    return <LoadingPage />
+    return <LoadingPage />;
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const res = await fetch(`/api/quotes/search?query=${query}`);
+    const quotes = await res.json();
+    setQuotes(quotes);
+    setLoading(false);
+  };
 
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search for Quotes..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="input input-bordered w-full max-w-xs"
+        />
+        <button type="submit" className="btn btn-primary">
+          Search
+        </button>
+      </form>
       {quotes.map((quote) => (
         <div key={quote.id}>
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <div className="card-body">
-            <p>{quote.quote}</p>
-            <p>- {quote.by}</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-error">Delete</button>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <p>{quote.quote}</p>
+              <p>- {quote.by}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-error">Delete</button>
+              </div>
             </div>
           </div>
-        </div>
-        <br />
+          <br />
         </div>
       ))}
     </div>
