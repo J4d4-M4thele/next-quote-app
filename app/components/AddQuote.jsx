2623bbcd535ad46d8a1eb82abf4cc171a20cf956
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-01-30 07:59:09 +0200

    update

--- app/components/AddQuote.jsx
+++ app/components/AddQuote.jsx
@@ -3,6 +3,27 @@ import { useState } from "react";
 
 const AddQuote = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [newQuote, setNewQuote] = useState("");
+  const [newQuoteBy, setNewQuoteBy] = useState("");
+
+  const handleSubmitNewQuote = async (e) => {
+    e.preventDefault();
+    const res = await fetch(`/api/quotes/`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        quote: newQuote,
+        by: newQuoteBy,
+      }),
+    });
+    if (res.ok) {
+      setNewQuote("");
+      setNewQuoteBy("");
+      setModalOpen(false);
+    }
+  };
 
   return (
     <div>
@@ -13,7 +34,11 @@ const AddQuote = () => {
         id="my_modal_3"
         className={`modal ${modalOpen ? "modal-open" : ""}`}
       >
-        <form method="dialog" className="modal-box">
+        <form
+          method="dialog"
+          className="modal-box"
+          onSubmit={handleSubmitNewQuote}
+        >
           <button
             onClick={() => setModalOpen(false)}
             htmlFor="my-modal-3"
@@ -22,11 +47,23 @@ const AddQuote = () => {
             ✕
           </button>
           <h3 className="font-bold text-lg">Add Quote</h3>
+          {/* quote input field */}
           <input
             type="text"
+            value={newQuote}
+            onChange={(e) => setNewQuote(e.target.value)}
             placeholder="Enter a new quote..."
             className="input input-bordered w-full max-w-xs"
           />
+          {/* by input field */}
+          <input
+            type="text"
+            value={newQuoteBy}
+            onChange={(e) => setNewQuoteBy(e.target.value)}
+            placeholder="Enter who the quote is by..."
+            className="input input-bordered w-full max-w-xs"
+          />
+          <br />
           <button type="submit" className="btn btn-primary">
             Add New Quote
           </button>
