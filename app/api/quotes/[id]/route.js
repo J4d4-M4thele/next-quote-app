d895393df911da8594f87ace60185e1ce384942e
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-01-30 11:32:51 +0200

    update

--- app/api/quotes/[id]/route.js
+++ app/api/quotes/[id]/route.js
@@ -1,13 +1,8 @@
 import { NextResponse } from "next/server";
-import quotes from '../data.json';
 
 export async function DELETE(request, {params}) {
     const id = params.id;
-    
-    const index = quotes.findIndex((quote) => quote.id === id);
-    if(index !== -1) {
-        quotes.splice(index, 1);
-    }
+    await prisma.quote.delete({where: {id: id}});
     
     return new NextResponse({"Book deleted": id});
 }
